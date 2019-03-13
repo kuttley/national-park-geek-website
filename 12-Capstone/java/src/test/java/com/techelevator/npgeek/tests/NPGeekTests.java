3ae132dd997e1aca738b279516845b815907d9b6
Quinnn Hebert <hebert.42@osu.edu>
2019-03-12 21:15:39 -0400

    added coments for the controller and model and also started to add unit tests

--- 12-Capstone/java/src/test/java/com/techelevator/npgeek/tests/NPGeekTests.java
+++ 12-Capstone/java/src/test/java/com/techelevator/npgeek/tests/NPGeekTests.java
@@ -3,7 +3,6 @@ package com.techelevator.npgeek.tests;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import javax.sql.DataSource;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
